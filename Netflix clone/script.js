3089bfe9d4ea9c62ade699805f5714a2ffe385f0
Mallikarjun <112100271+Mallu13sp@users.noreply.github.com>
2024-01-27 16:32:48 +0530

    Add files via upload

---
+++ Netflix clone/script.js
@@ -0,0 +1,8 @@
+function openNewPage1() {
+    var newPageUrl = 'https://www.netflix.com/in/login'; 
+    window.open(newPageUrl, '_self');
+}
+function openNewPage2() {
+    var newPageUrl = 'https://www.netflix.com/signup/'; 
+    window.open(newPageUrl, '_self');
+}
